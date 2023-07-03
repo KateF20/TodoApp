a7f9b8b3824daf3bc3f5f08f06fe83456eeed02e
Ekaterina Fedoseeva <kaitherinayrd@gmail.com>
2023-07-03 16:00:22 +0200

    admin router and authentication added endpoint names changed in todos.py

--- routers/todos.py
+++ routers/todos.py
@@ -32,7 +32,7 @@ class TodoModel(BaseModel):
 
 
 @router.get('/', status_code=status.HTTP_200_OK)
-async def read_all(user: user_dependency, db: db_dependency):
+async def get_all_todos(user: user_dependency, db: db_dependency):
     if user is None:
         raise HTTPException(status_code=401, detail='Authentication Failed')
 
@@ -40,7 +40,7 @@ async def read_all(user: user_dependency, db: db_dependency):
 
 
 @router.get('/todo/{todo_id}', status_code=status.HTTP_200_OK)
-async def read_todo(user: user_dependency, db: db_dependency, todo_id: int = Path(gt=0)):
+async def get_todo_by_id(user: user_dependency, db: db_dependency, todo_id: int = Path(gt=0)):
     if user is None:
         raise HTTPException(status_code=status.HTTP_201_CREATED, detail='Authentification failed')
 
@@ -61,7 +61,7 @@ async def create_todo(user: user_dependency, db: db_dependency, todo_model: Todo
 
 
 @router.put('/todo/{todo_id}', status_code=status.HTTP_204_NO_CONTENT)
-async def read_all(user: user_dependency, db: db_dependency,
+async def update_todo(user: user_dependency, db: db_dependency,
                    todo_model: TodoModel,
                    todo_id: int = Path(gt=0)):
     if user is None:
@@ -81,7 +81,7 @@ async def read_all(user: user_dependency, db: db_dependency,
 
 
 @router.delete('/todo/{todo_id}', status_code=status.HTTP_204_NO_CONTENT)
-async def read_all(user: user_dependency, db: db_dependency, todo_id: int = Path(gt=0)):
+async def delete_todo(user: user_dependency, db: db_dependency, todo_id: int = Path(gt=0)):
     if user is None:
         raise HTTPException(status_code=401, detail='Authentication Failed')
 
