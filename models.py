341de0d6185f59ab67b405d9d1cd4b1f8cd3da66
Ekaterina Fedoseeva <kaitherinayrd@gmail.com>
2023-07-05 16:40:53 +0200

    moving all db information into the database file renaming Todo model

--- models.py
+++ models.py
@@ -3,7 +3,7 @@ from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
 from database import Base
 
 
-class Todos(Base):
+class Todo(Base):
     __tablename__ = 'todos'
 
     id = Column(Integer, primary_key=True, index=True)
