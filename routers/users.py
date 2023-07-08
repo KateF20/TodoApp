08f668674908765e70313e8f366c3c02e65c6f23
Ekaterina Fedoseeva <kaitherinayrd@gmail.com>
2023-07-08 15:21:17 +0200

    minor changes to the project

--- routers/users.py
+++ routers/users.py
@@ -1,8 +1,8 @@
 from starlette import status
 from pydantic import BaseModel, Field
-from fastapi import HTTPException, Path, APIRouter
+from fastapi import HTTPException, APIRouter
 
-from models import Todo, User
+from models import User
 from dependencies import user_dependency, db_dependency_type, bcrypt_context
 
 
